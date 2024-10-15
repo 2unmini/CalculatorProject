85337a9bc49a353e1eb085d1718c27ffc60cba8a
zunmin <2unmin4030@gmail.com>
2024-10-15 14:26:21 +0900

    Feat: 사칙 연산후 나온 결과를 기록 및 삭제 기능 추가
    사칙연산 후 나온 결과를 ArrayList를 통해 저장하고 remove 입력 시 가장 먼저 저장된 결과를 기능을 추가
    result를 직접 접근하지 못하고 private으로 변경 후 getter, setter로 접근 하게 변경

--- src/calculator/App.java
+++ src/calculator/App.java
@@ -1,8 +1,9 @@
 package calculator;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
-public class SimpleCalculator {
+public class App {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         Calculator calculator = new Calculator();
@@ -14,12 +15,18 @@ public class SimpleCalculator {
             int num2 =sc.nextInt();
             System.out.println("사칙 연산 기호를 입력 : ");
             char oper=sc.next().charAt(0);
-            calculator.calculate(num1,num2,oper);
+            calculator.setResult(calculator.calculate(num1,num2,oper));
+            System.out.println("결과: "+calculator.getResult());
             System.out.println("더 계산 하시겠습니까? (exit 입력시 종료)");
+            System.out.println("가장 먼저 저장된 데이터를 삭제하시겠습니까? (remove 입력시 종료)");
             String exit =sc.next();
             if(exit.equals("exit")){
                 return;
             }
+            if(exit.equals("remove")){
+                calculator.removeResult();
+            }
+
         }
 
     }
--- src/calculator/Calculator.java
+++ src/calculator/Calculator.java
@@ -1,21 +1,30 @@
 package calculator;
 
+import java.util.ArrayList;
+
 public class Calculator {
+    ArrayList<Integer> list = new ArrayList<>();
     private int result;
 
-    public void calculate(int num1,int num2,char oper){
+    public int getResult() {
+        return result;
+    }
+
+    public void setResult(int result) {
+        this.result = result;
+    }
+
+    public int calculate(int num1, int num2, char oper){
+        Calculator calculator = new Calculator();
         switch (oper) {
             case '+':
                 result= num1+num2;
-                System.out.println("결과: "+result);
                 break;
             case '-':
                 result= num1-num2;
-                System.out.println("결과: "+result);
                 break;
             case '*':
                 result= num1*num2;
-                System.out.println("결과: "+result);
                 break;
             case '/':
                 if (num2 == 0) {
@@ -23,9 +32,16 @@ public class Calculator {
                     break;
                 }else {
                     result= num1/num2;
-                    System.out.println("결과: "+result);
                     break;
                 }
         }
+        list.add(result);
+        return result;
+    }
+
+    public void removeResult(){
+        Calculator calculator = new Calculator();
+        System.out.println(list.remove(calculator.getResult())+" 가 제거 되었습니다.");
+        System.out.println("현재 기록: " + list.toString());
     }
 }

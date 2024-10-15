c71ef53adf8d21a8ed48c2714773b886d74530dd
zunmin <2unmin4030@gmail.com>
2024-10-15 17:36:55 +0900

    Fix: 사칙 연산후 나온 결과를 기록 및 삭제 기능 수정 및 히스토리 기능 추가
    사칙연산 후 나온 결과를 ArrayList를 통해 저장하고 remove 입력 시 가장 먼저 저장된 결과를 출력하는 기능을 수정
    list에 직접 접근하지 못하게 private으로 변경후 getter,setter로 접근하게 수정

--- src/calculator/App.java
+++ src/calculator/App.java
@@ -1,12 +1,12 @@
 package calculator;
 
-import java.util.ArrayList;
 import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         Calculator calculator = new Calculator();
+
 
         while (true){
             System.out.println("첫 번째 정수 입력 : ");
@@ -15,19 +15,22 @@ public class App {
             int num2 =sc.nextInt();
             System.out.println("사칙 연산 기호를 입력 : ");
             char oper=sc.next().charAt(0);
-            calculator.setResult(calculator.calculate(num1,num2,oper));
-            System.out.println("결과: "+calculator.getResult());
+            System.out.println("결과: "+calculator.calculate(num1,num2,oper));
+            System.out.println("기록: "+calculator.getList());
+            System.out.println("가장 먼저 저장된 데이터를 삭제하시겠습니까? (remove 입력시 삭제)");
+            String removeData =sc.next();
+            if(removeData.equals("remove")){
+                calculator.removeResult();
+            }
+
             System.out.println("더 계산 하시겠습니까? (exit 입력시 종료)");
-            System.out.println("가장 먼저 저장된 데이터를 삭제하시겠습니까? (remove 입력시 종료)");
+
             String exit =sc.next();
             if(exit.equals("exit")){
                 return;
             }
-            if(exit.equals("remove")){
-                calculator.removeResult();
-            }
-
         }
-
     }
+
+
 }
--- src/calculator/Calculator.java
+++ src/calculator/Calculator.java
@@ -3,19 +3,21 @@ package calculator;
 import java.util.ArrayList;
 
 public class Calculator {
-    ArrayList<Integer> list = new ArrayList<>();
+    private ArrayList<Integer> list = new ArrayList<>();
+
+    public ArrayList<Integer> getList() {
+        return list;
+    }
+
+    public void setList(ArrayList<Integer> list) {
+        this.list = list;
+    }
+
     private int result;
 
-    public int getResult() {
-        return result;
-    }
 
-    public void setResult(int result) {
-        this.result = result;
-    }
 
     public int calculate(int num1, int num2, char oper){
-        Calculator calculator = new Calculator();
         switch (oper) {
             case '+':
                 result= num1+num2;
@@ -40,8 +42,9 @@ public class Calculator {
     }
 
     public void removeResult(){
-        Calculator calculator = new Calculator();
-        System.out.println(list.remove(calculator.getResult())+" 가 제거 되었습니다.");
+        int remove =list.get(0);
+        list.remove(list.get(0));
+        System.out.println(remove+" 가 제거 되었습니다.");
         System.out.println("현재 기록: " + list.toString());
     }
 }
